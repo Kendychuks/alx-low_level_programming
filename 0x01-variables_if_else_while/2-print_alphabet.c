b2a281696a9717cc5e6e1454658c120a2ae50e65
kendychuks <kendychuksroyal@gmail.com>
2022-12-09 16:44:53 -0800

    print

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,16 +5,16 @@
  *  * main - returns alphabets
  *   *
  *    * Return: Always 0 (Success)
- *     **/
+ *     */
 int main(void)
 {
 		int ch = 'a';
 
-		while (ch <= 'z'\n')
-				{
+		while (ch <= 'z')
+					{
 								putchar(ch);
 										ch++;
 											}
-		putchar('\n');
-		return (0);
+				putchar('\n');
+					return (0);
 }
