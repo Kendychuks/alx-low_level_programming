1c6de019eb53cc7e9e729dbca3cde2863676836c
kendychuks <kendychuksroyal@gmail.com>
2022-12-15 19:23:59 -0800

    task 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -2,27 +2,33 @@
 /**
  * print_square - prints a square
  * @size: print size
- * Return : 0
+ * Return: Always 0.
  */
 
 void print_square(int size)
 {
-	int i;
-	int k;
 
-	if (size > 0)
-	{
-		for (i = 0; i < size; i++)
-		{
-			for (k = 0; k < size; k++)
-			{
-				_putchar (35);
-			}
-			_putchar ('\n'n);
-		}
-	}
-	else
-	{
-		_putchar ('\n');
-	}
+int i;
+int k;
+
+if (size > 0)
+{
+for (i = 0; i < size; i++)
+{
+
+for (k = 0; k < size; k++)
+{
+_putchar (35);
+}
+_putchar ('\n');
+
+}
+
+}
+
+else
+{
+_putchar ('\n');
+}
+
 }
